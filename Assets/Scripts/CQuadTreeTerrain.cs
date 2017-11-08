67e5022b417d3c77896dbe1da4a4cfa069118689
jonbinliu <jonbinliu@hotmail.com>
2017-11-09 02:02:34 +0800

    Init

---
+++ Assets/Scripts/CQuadTreeTerrain.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine; 
+
+
+namespace Assets.Scripts.QuadTree
+{
+    struct stHeightData
+    {
+        private ushort[,] heightData;
+        public int size;    
+        
+        public bool IsValid()
+        {
+            return heightData != null; 
+        }    
+        
+        public void Release()
+        {
+            heightData = null;
+            size = 0; 
+        }   
+
+
+        public void Allocate( int mapSize )
+        {
+            if( mapSize > 0 )
+            {
+                heightData = new ushort[mapSize,mapSize];
+                size = mapSize; 
+            }
+        }
+    }
+
+
+    class CQuadTreeTerrain
+    {
+        private stHeightData mHeightData;
+
+
+        public void UnloadHeightMap()
+        {
+            mHeightData.Release();
+
+            Debug.Log("Height Map is Unload!"); 
+        }
+
+
+        public bool MakeTerrainFault( int size , int iter , int minHeightValue , int maxHeightValue , float filter )
+        {
+            if( mHeightData.IsValid() )
+            {
+                UnloadHeightMap();      
+            }
+
+            mHeightData.Allocate(size); 
+
+            return false ; 
+        }
+    }
+}
---
+++ Assets/Scripts/DemoFramework.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+
+public class DemoFramework : MonoBehaviour {
+
+    public GameObject terrainGo;
+
+    public int heightSize;
+
+    //是否从高度图读取高度信息
+    //True从文件读取
+    //False动态生成
+    public bool isLoadHeightDataFromFile;
+    public string heightFileName;
+
+    public bool isGenerateHeightDataRuntime;
+    public int iterations;
+    public ushort minHeightValue;
+    public ushort maxHeightValue;
+    public float filter; 
+    
+   
+    //1、读取高度图，
+    //2、设置顶点间距离，
+    //3、读取纹理
+    //4、设置光照阴影
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
