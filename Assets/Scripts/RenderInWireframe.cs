eede969eb962f3c2361d1ca514dfb0930b34e280
jonbinliu <jonbinliu@hotmail.com>
2017-11-16 09:56:55 +0800

    Wireframe Control

---
+++ Assets/Scripts/RenderInWireframe.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RenderInWireframe : MonoBehaviour
+{
+    public bool wireframeMode = false;
+
+    void OnPreRender()
+    {
+        GL.wireframe = true;
+    }
+    void OnPostRender()
+    {
+        GL.wireframe = false;
+    }
+}
